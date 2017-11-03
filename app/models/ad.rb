4e2280ed80b14422b17d339aa66fe973ca535dd8
Sean Biganski <sean@hellolabs.com>
2017-11-03 10:10:20 -0400

    Paperclip hackery to get it to accept my png files
    Also more user removal

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,11 +2,10 @@ class Ad < ApplicationRecord
 
   has_many :ad_activities
 
-  has_attached_file :file
+  has_attached_file :file, { validate_media_type: false }
 
   validates :title, length: { in: 5..50 }
   validates :file, presence: true
-
-  do_not_validate_attachment_file_type :file
+  validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
